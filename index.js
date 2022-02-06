b0fa41687aef5170b4612fad0d52d0b77352150f
Hillary Muller <hillaryaward@gmail.com>
2022-02-06 14:38:20 -0500

    commit tree 24df25a8a437760e83d4f66e8afff3d8cf5c0feb

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(){
+    return 1 + 2;
+}
+function subtract(){
+    return 2 - 1;
+}
+function multiply(){
+    return 2 * 3;
+}
+function divide(){
+    return 5 / 2;
+}
+function add(a, b){
+    return a + b;
+}
+function subtract(a, b){
+    return a - b;
+}
+function multiply(a, b){
+    return a * b;
+}
+function divide(a, b){
+    return a / b;
+}
+function increment(n){
+    return ++n;
+}
+function decrement(n){
+    return --n;
+}
+function makeInt(n){
+    return parseInt(n, 10);
+}
+function preserveDecimal(n){
+    return parseFloat(n);
+}
